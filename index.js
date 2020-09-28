1c1239e7847f74ab5fce16ac4ccd975be2fc58de
shreya0181 <shreya171769.cse@chitkara.edu.in>
2020-09-28 11:33:43 +0530

    adding reducer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Buy_cake = "Buy Cake"
 // creating an Action 
 
+// *** Applications state has to be represented by a single object
 
+const initial_state = 10;
 function buycake ()
 {
      // An action has a type property 
@@ -16,3 +18,16 @@ function buycake ()
 
 
 // (prevState, action )=> newState { } *** reducers work
+
+const reducer = (state= initial_state, action)=>{
+    switch(action.type){
+        case Buy_cake : return{
+            //  spread makes a copy of state and  then updates the copy 
+            ...state, 
+            numberOfcake: state.numberOfcake -1
+
+        }
+         default : return state
+    }
+
+}
