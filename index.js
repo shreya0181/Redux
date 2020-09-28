2276c6dfbcdd694f361aae4938028768aee92816
shreya0181 <shreya171769.cse@chitkara.edu.in>
2020-09-28 11:13:52 +0530

    Adding action

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("from index");
+const Buy_cake = "Buy Cake"
+// creating an Action 
+
+
+function buycake ()
+{
+     // An action has a type property 
+    return 
+    {
+        type:  Buy_cake
+        info:'firstReduxAction'
+    
+    }
+}
+//  Action creator is a function that returns an action 
+
+
+// (prevState, action )=> newState { } *** reducers work
