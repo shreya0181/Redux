4b3d5633a4241e7d961581afdda79649a099ba08
shreya0181 <shreya171769.cse@chitkara.edu.in>
2020-09-28 14:34:50 +0530

    adding dispatcher, and subscribe

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+const { createStore } = require("redux");
+
+const redux =require('redux');
 const Buy_cake = "Buy Cake"
 // creating an Action 
 
 // *** Applications state has to be represented by a single object
 
-const initial_state = 10;
+const initial_state ={numberOfcake: 10} ;
 function buycake ()
 {
      // An action has a type property 
-    return 
+    return (
     {
-        type:  Buy_cake
+        type:  Buy_cake,
         info:'firstReduxAction'
     
-    }
+    })
 }
 //  Action creator is a function that returns an action 
 
@@ -21,13 +24,26 @@ function buycake ()
 
 const reducer = (state= initial_state, action)=>{
     switch(action.type){
-        case Buy_cake : return{
+        case Buy_cake : return {
             //  spread makes a copy of state and  then updates the copy 
             ...state, 
-            numberOfcake: state.numberOfcake -1
+            numberOfcake: state.numberOfcake - 1
 
         }
          default : return state
     }
 
 }
+
+const store = createStore(reducer);
+    console.log("INitialState", store.getState());
+
+// register using subscribe
+    const unsubscribe = store.subscribe(()=>console.log("updated State", store.getState()));
+
+
+//Dispatcher is   used  pass actions 
+store.dispatch(buycake());
+
+// remember to unsubscirbe 
+unsubscribe();
