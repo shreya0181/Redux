a6eed158d29a982ab94e7e3882201768db1f13be
shreya0181 <shreya171769.cse@chitkara.edu.in>
2020-09-29 11:23:32 +0530

    adding AsynAction SetUp

---
+++ asyncActions.js
@@ -0,0 +1,58 @@
+const redux = require( 'redux');
+const createStore = redux.createStore;
+const initalState ={
+loading : false,
+users : [],
+error :' '
+}
+
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+
+const fetechUsersRequest = ()=>{
+  return { 
+      type: FETCH_USERS_REQUEST
+    
+}}
+
+const fetchUsersSuccess = ()=>{
+    return {
+        type :  FETCH_USERS_SUCCESS, 
+        payload : users 
+    }
+}
+
+const fetchUsersFailure = ()=>{
+    return{
+        type :  FETCH_USERS_FAILURE, 
+        payload : error
+    }
+}
+
+const reducer = (state = initalState, action)=>{
+ switch(action.type)
+ { 
+     case FETCH_USERS_REQUEST: return{
+
+        ...state, loading: true
+     }
+
+     case FETCH_USERS_SUCCESS: return{
+         loading:false,
+         users:action.payload,
+         error :''
+
+     }
+     case FETCH_USERS_FAILURE: return {
+         loading : false,
+         users: [],
+         error: action.payload
+
+     }
+ }
+}
+const store = createStore(reducer);
+// store.subscribe(()=>{});
+// store.dispatch();
