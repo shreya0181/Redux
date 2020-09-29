0850c084d75b6834b4314459dc4784f2b5c25ea3
shreya0181 <shreya171769.cse@chitkara.edu.in>
2020-09-29 09:04:04 +0530

    Single reducer withe multiple actions

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { createStore } = require("redux");
 
 const redux =require('redux');
-const Buy_cake = "Buy Cake"
+const Buy_cake = "Buy Cake";
+const Buy_IceCream = " Buy  IceCream"
 // creating an Action 
 
 // *** Applications state has to be represented by a single object
 
-const initial_state ={numberOfcake: 10} ;
+const initial_state ={numberOfcake: 10,
+numberOfIceCreams: 20} ;
 function buycake ()
 {
      // An action has a type property 
@@ -19,6 +21,17 @@ function buycake ()
 }
 //  Action creator is a function that returns an action 
 
+function buyIceCream ()
+{
+     // An action has a type property 
+    return (
+    {
+        type:  Buy_IceCream,
+        info:'secondReduxAction'
+    
+    })
+}
+
 
 // (prevState, action )=> newState { } *** reducers work
 
@@ -30,6 +43,12 @@ const reducer = (state= initial_state, action)=>{
             numberOfcake: state.numberOfcake - 1
 
         }
+        case Buy_IceCream : return {
+            //  spread makes a copy of state and  then updates the copy 
+            ...state, 
+            numberOfIceCreams: state.numberOfIceCreams - 1
+
+        }
          default : return state
     }
 
@@ -38,12 +57,13 @@ const reducer = (state= initial_state, action)=>{
 const store = createStore(reducer);
     console.log("INitialState", store.getState());
 
-// register using subscribe
+// register using subscribe--- *listener
     const unsubscribe = store.subscribe(()=>console.log("updated State", store.getState()));
 
 
 //Dispatcher is   used  pass actions 
 store.dispatch(buycake());
+store.dispatch(buyIceCream ());
 
 // remember to unsubscirbe 
 unsubscribe();
